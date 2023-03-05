1de2962a9e4fad6def8bcd5ffa031ca444bd9116
Snježana007 <120574058+Snjezana007@users.noreply.github.com>
2023-03-05 20:24:13 +0100

    change css to sass

--- Desktop/react-vjezbe/vjezba-inicijalizacija/src/App.jsx
+++ Desktop/react-vjezbe/vjezba-inicijalizacija/src/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <h1>Polaznik: Snježana Mikša</h1>
     </div>
   );
 }
